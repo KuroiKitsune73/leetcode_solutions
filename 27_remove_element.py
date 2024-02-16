4df4e1620577a693a393ee9f817acf3a1ded9229
Kuroi Kitsune <klerry.no.tyan@gmail.com>
2024-02-16 22:54:16 +0300

    Task 27 Remove Element (Easy)

---
+++ 27_remove_element.py
@@ -0,0 +1,12 @@
+'''
+https://leetcode.com/problems/remove-element/description/
+Time: 20ms
+Memory: 11.62 Mb
+'''
+class Solution(object):
+    def removeElement(self, nums, val):    
+        for i in range(len(nums)+1):
+            if val in nums:
+                nums.remove(val)
+                i+=1
+        print(len(nums))
