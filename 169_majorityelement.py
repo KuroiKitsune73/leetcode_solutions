9b17618a18de071c05b226818cae6b71af79ed0e
KuroiKitsune73 <klerry.no.tyan@gmail.com>
2024-02-18 23:12:58 +0300

    169 Task Majority Element Easy

---
+++ 169_majorityelement.py
@@ -0,0 +1,12 @@
+'''
+https://leetcode.com/problems/majority-element/description/
+'''
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+        return candidate
